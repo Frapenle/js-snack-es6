cbd1a68b654852d040df25b8df35f2b1cee7cb3c
francescotrudu <francescotrudu84@gmail.com>
2022-12-05 12:39:24 +0100

    esercizio zucchina

---
+++ script3.js
@@ -0,0 +1,62 @@
+/**
+ *
+ * Crea 10 oggetti che rappresentano una zucchina, 
+ * indicandone per ognuno varietà, peso e lunghezza.
+ *
+ * Calcola quanto pesano tutte le zucchine.
+ *
+ */
+
+const zucchina = [{
+   varieta: "romana",
+   peso: 100,
+   lunghezza: 20,
+},
+{
+   varieta: "verde",
+   peso: 150,
+   lunghezza: 30,
+},
+{
+   varieta: "verdeScuro",
+   peso: 120,
+   lunghezza: 10,
+},
+{
+   varieta: "siciliana",
+   peso: 150,
+   lunghezza: 15,
+},
+{
+   varieta: "lombarda",
+   peso: 150,
+   lunghezza: 30,
+},
+{
+   varieta: "sarda",
+   peso: 150,
+   lunghezza: 30,
+},
+{
+   varieta: "campana",
+   peso: 150,
+   lunghezza: 30,
+},
+{
+   varieta: "bolognese",
+   peso: 150,
+   lunghezza: 30,
+},
+{
+   varieta: "milanese",
+   peso: 150,
+   lunghezza: 30,
+}]
+
+let sum = 0;
+for ( let i = 0; i < zucchina.length; i++) {
+   const currentZucchina = zucchina[i];
+   sum += currentZucchina.peso;
+}
+
+console.log(sum)
